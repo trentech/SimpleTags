cc2c8f62e9c94b57b7b9b85c74fe75b0004a2157
TrenTech@gmail.com <TrenTech@gmail.com>
2016-06-10 10:49:56 -0400

    SpongeGradle

--- src/main/java/com/gmail/trentech/simpletags/utils/Resource.java
+++ src/main/java/com/gmail/trentech/simpletags/utils/Resource.java
@@ -3,10 +3,9 @@ package com.gmail.trentech.simpletags.utils;
 public class Resource {
 
 	public final static String NAME = "Simple Tags";
-	public final static String VERSION = "0.2.5b";
+	public final static String VERSION = "@project.version@";
 	public final static String ID = "com.gmail.trentech.simpletags";
 	public final static String AUTHOR = "TrenTech";
 	public final static String URL = "http://github.com/TrenTech/SimpleTags";
-	public final static String DESCRIPTION = "Add custom tags to player chat";
 	
 }
