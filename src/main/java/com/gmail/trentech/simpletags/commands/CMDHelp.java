87a7ce2b7e1952c64b78832d16b0ba0390222d3d
TrenTech <TrenTech@gmail.com>
2016-10-15 23:07:23 -0400

    fix help

--- src/main/java/com/gmail/trentech/simpletags/commands/CMDHelp.java
+++ src/main/java/com/gmail/trentech/simpletags/commands/CMDHelp.java
@@ -10,6 +10,14 @@ import com.gmail.trentech.simpletags.utils.Help;
 
 public class CMDHelp implements CommandExecutor {
 
+	public CMDHelp() {
+		new Help("tag help", "help", "Get help with all commands in SimpleTags", false)
+			.setPermission("stackban.cmd.sban")
+			.setUsage("/tag help <rawCommand>")
+			.setExample("/tag help tag create")
+			.save();
+	}
+	
 	@Override
 	public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
 		Help help = args.<Help> getOne("command").get();
