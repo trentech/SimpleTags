2a4db331d91a392db9e4e0d228e702458a1c9f70
TrenTech <TrenTech@gmail.com>
2016-10-31 23:45:43 -0400

    Improve /say listener

--- src/main/java/com/gmail/trentech/simpletags/EventListener.java
+++ src/main/java/com/gmail/trentech/simpletags/EventListener.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
@@ -20,7 +21,11 @@ import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.Text.Builder;
 import org.spongepowered.api.text.TextTemplate;
 import org.spongepowered.api.text.action.TextActions;
+import org.spongepowered.api.text.channel.MessageChannel;
+import org.spongepowered.api.text.channel.MessageReceiver;
+import org.spongepowered.api.text.channel.MutableMessageChannel;
 import org.spongepowered.api.text.format.TextColors;
+import org.spongepowered.api.text.selector.Selector;
 import org.spongepowered.api.text.serializer.TextSerializers;
 
 import com.gmail.trentech.simpletags.events.ChangeTagEvent;
@@ -84,31 +89,66 @@ public class EventListener {
 	@Listener
 	public void onSendCommandEvent(SendCommandEvent event, @First CommandSource src) {
 		String command = event.getCommand();
+		
 		if (!command.equalsIgnoreCase("say")) {
 			return;
 		}
 
-		Text message = TextSerializers.FORMATTING_CODE.deserialize(event.getArguments());
+		if(Sponge.getPluginManager().isLoaded("simplechat")) {
+			return;
+		}
+
+		String message = event.getArguments();
 		
+		if(message == null || message.equals("")) {
+			return;
+		}
+
+		MutableMessageChannel channel = MessageChannel.TO_ALL.asMutable();
+		
+		if(message.startsWith("@")) {
+			channel = MessageChannel.TO_NONE.asMutable();
+
+			String selector = message.substring(0, message.indexOf(" "));
+			
+			for(Entity entity : Selector.parse(selector).resolve(src)) {
+
+				if(entity instanceof Player) {
+					channel.addMember((MessageReceiver) entity);
+				}
+			}
+			
+			if(channel.getMembers().size() == 0) {
+				src.sendMessage(Text.of(TextColors.RED, "Selector returned no players"));
+				return;
+			}
+			
+			channel.addMember(Sponge.getServer().getConsole());
+			
+			message = message.replace(selector, "");
+		}
+		
+		Text text = TextSerializers.FORMATTING_CODE.deserialize(message);
+
 		if(src instanceof Player) {
 			Player player = (Player) src;
 			
 			Optional<PlayerTag> optionalPlayerTag = PlayerTag.get(player);
 
 			if (optionalPlayerTag.isPresent()) {
-				Sponge.getServer().getBroadcastChannel().send(Text.of(optionalPlayerTag.get().getTag(), " ", message));
+				text = Text.join(optionalPlayerTag.get().getTag(), Text.of(" "), text);
 			} else {
-				Sponge.getServer().getBroadcastChannel().send(Text.of(PlayerTag.getDefault(player), " ", message));
+				text = Text.join(PlayerTag.getDefault(player), Text.of(" "), text);
 			}
 		} else {
-			Optional<SingleTag> optionalTag = SingleTag.get(Main.getPlugin().getId(), "console");
+			Optional<SingleTag> optionalTag = SingleTag.get("simpletags", "console");
 			
 			if(optionalTag.isPresent()) {
-				Sponge.getServer().getBroadcastChannel().send(Text.of(optionalTag.get().getTag(), " ", message));
-			} else {
-				Sponge.getServer().getBroadcastChannel().send(Text.of(message));
+				text = Text.join(optionalTag.get().getTag(), Text.of(" "), text);
 			}
 		}
+
+		channel.send(text);
 
 		event.setCancelled(true);
 	}
